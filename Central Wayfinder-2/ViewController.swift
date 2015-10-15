621af6bedec43381a98e60705bb243fb39444f1c
Lucas Angelon <lucas.a.arouca@gmail.com>
2015-10-15 09:10:39 +0800

    Initial Commit

---
+++ Central Wayfinder-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Central Wayfinder-2
+//
+//  Created by Lucas Angelon Arouca on 15/10/2015.
+//  Copyright © 2015 Lucas Angelon Arouca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
