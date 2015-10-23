4bd0fbe7ed9a53d431750316a52b9943c4c3975e
Lucas Angelon <lucas.a.arouca@gmail.com>
2015-10-23 10:53:34 +0800

    Update SettingsViewController
    Added the actual tableView properly set up with the spacing between sections as well as the segues to and from the campus selection. Also added CampusSelectionViewController to the project and the storyboard.

---
+++ Central Wayfinder-2/CampusSelectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CampusSelectionViewController.swift
+//  Central Wayfinder-2
+//
+//  Created by Lucas Angelon Arouca on 23/10/2015.
+//  Copyright © 2015 Lucas Angelon Arouca. All rights reserved.
+//
+
+import UIKit
+
+class CampusSelectionViewController : ViewController {
+    
+}
--- Central Wayfinder-2/SettingsViewController.swift
+++ Central Wayfinder-2/SettingsViewController.swift
@@ -8,17 +8,63 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // Declaring the base tableView.
+    @IBOutlet var tableView: UITableView!
+    
+    // List items for the menu.
+    let cellContent = [["Accessibility", "Select Campus"], ["About", "Terms of Service", "Copyright"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return cellContent.count
+    }
+    
+    // Setting up the color for the footers.
+    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = UIView(frame: CGRectMake(0, 0, tableView.frame.size.width, 10))
+        
+        if section == 0 {
+            footerView.backgroundColor = UIColor.grayColor().colorWithAlphaComponent(0.1)
+        } else {
+            footerView.backgroundColor = UIColor.whiteColor()
+        }
+        
+        
+        return footerView
+    }
+    
+    // Returns the item count from the list based on the cellContent variable.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellContent[section].count
+    }
+    
+    // Returns the properly set up items for the list.
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("TextCell", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = cellContent[indexPath.section][indexPath.row]
+        
+        return cell
+    }
+    
+    // handling the clicks on the table items.
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        switch indexPath.row {
+            
+        // When the "Services" item is clicked.
+        case 1:
+            self.performSegueWithIdentifier("ShowCampusSelectionViewController", sender: self)
+            
+        // Default action.
+        default:
+            print("NoSegueAvailable")
+        }
     }
 }
 
